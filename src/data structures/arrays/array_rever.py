2599371b0ddc069b270c643578024a54a6d870c2
Mylena Santos <2214212@aluno.univesp.br>
2024-05-15 14:57:32 -0300

    exercises array

---
+++ src/data structures/arrays/array_rever.py
@@ -0,0 +1,9 @@
+# Defining a function to reverse the array
+def reverse_array(array):
+    return array[::-1]
+
+# Example
+my_array = [1, 2, 3, 4, 5]
+reversed_array = reverse_array(my_array)
+print("Original array:", my_array)
+print("Reversed array:", reversed_array)
